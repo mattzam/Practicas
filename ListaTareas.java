db7429c5c918058b243db774c560954da9479736
Matthew Zamudio <143620932+mattzam@users.noreply.github.com>
2023-09-06 08:34:42 -0600

    Add files via upload

---
+++ ListaTareas.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package variables;
+
+/**
+ *
+ * @author expec
+ */
+//psvm para main
+public class ListaTareas {
+    ListaTareas() {
+            String t1,t2,t3,t4,t5;
+    t1 = "Exposicion de Intro. a TICS";
+    t2 = "Exposicion Taller de Etica";
+    t3 = "Exposicion de Fundamentos de programaciOn";
+    t4 = "Serie Ejercicios Mates Discretas";
+    t5 = "Serie de Ejercicios Desigualdades Cuadraticas y Valor Absoluto";   
+        System.out.println("Mi tarea 1 es: "+t1);
+        System.out.println("Mi tarea 2 es: "+t2);
+        System.out.println("Mi tarea 3 es: "+t3);
+        System.out.println("Mi tarea 4 es: "+t4);
+        System.out.println("Mi tarea 5 es: "+t5);
+    }
+}
---
+++ Variables.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package variables;
+
+/**
+ *
+ * @author expec
+ */
+public class Variables {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int edad;
+        String nombre = "Matt"; //Este es mi nombre
+        String materia = "Fundamentos de Programacion";
+        String nombreMascota = "Gatito";
+        final int anoNacimiento = 2000;
+        String cadenaTexto = "cadena de texto !!!!%%$$";
+        System.out.println("Ola mi nombre es: "+nombre);
+        System.out.println("Ola mi nombre es: "+nombre+" y estudio: "+materia);
+                ListaTareas listaTareas = new ListaTareas();
+    }
+    
+}
